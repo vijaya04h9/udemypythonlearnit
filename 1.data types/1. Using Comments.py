ae8244cb6704773663465f9de2694ed6a9092bff
Vitalii Shumylo <vitalishumi@gmail.com>
2024-03-09 13:55:56 +0200

    initial

---
+++ 1.data types/1. Using Comments.py
@@ -0,0 +1,48 @@
+# This is a single-line comment
+x = 10  # Assign the value of 10 to the variable x
+y = 20  # Assign the value of 20 to the variable y
+
+# Calculate the sum of x + y
+result = x + y
+
+print(result)
+
+# This is a multi-line comment
+# created by using multiple
+# single-line comments.
+
+"""
+This is a multi-line comment
+created by using string literals using double-quotes.
+"""
+
+'''
+This is a multi-line comment
+created by using string literals using single-quotes.
+'''
+
+# Commented out code
+x = 10  # Assign the value of 10 to the variable x
+y = 20  # Assign the value of 20 to the variable y
+
+# Calculate the sum of x + y
+result = x + y
+
+print(result)
+
+# PyCharm shortcuts
+
+# Ctrl + /
+# To comment code, place the caret at the appropriate line and press Ctrl + /
+# Press Ctrl + / again on the same line to uncomment it.
+
+# Shift + Alt + . to make the font size larger
+
+# Shift + Alt + , to make the font size smaller
+
+# Ctrl + d to duplicate a line
+
+
+
+
+
