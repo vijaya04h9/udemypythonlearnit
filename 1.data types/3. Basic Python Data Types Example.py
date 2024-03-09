61e28ce95ec080850df5a8fd4b427cc2a8185cc0
Vitalii Shumylo <vitalishumi@gmail.com>
2024-03-09 14:05:56 +0200

    initial

--- 1.data types/3. Basic Python Data Types Example.py
+++ 1.data types/3. Basic Python Data Types Example.py
@@ -1,1 +1,11 @@
-#
+# Personal information
+name = "Helen"  # Str
+age = 27  # Int
+height_meters = 1.6  # Float
+is_student = False   # Boolean
+
+# Print out the information
+print('Name:', name)
+print('Age:', age)
+print('Height:', height_meters, "meters")
+print("Are you a student?", is_student)
