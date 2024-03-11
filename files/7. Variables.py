7ddc4436d356e38a6ec671b8551d4835730c65ed
Vitalii Shumylo <vitalishumi@gmail.com>
2024-03-11 19:24:33 +0200

    initial

---
+++ files/7. Variables.py
@@ -0,0 +1,32 @@
+# Variables
+first_name = "John"
+Sport = 'baseball'  # capital 'S'
+print(sport)  # Lowercase 'S'
+
+num1 = 5  # storing an integer into a variable
+num2 = 8.4  # storing a float into a variable
+
+print(num1, num2)  # you can print multiple items using commas
+
+# Storing a boolean into a variable
+switch = True
+
+print(switch)
+
+# Storing strings into a variable
+name = 'John Smith'
+
+fav_number = '9'
+print(name, fav_number)  # will print 9 next to the name
+
+# Using two variables to create another variable
+result = num1 + num2
+
+print(result)
+
+# Adding, deleting, multiplying, dividing from a variable
+result += 1  # same as saying result = result + 1
+print(result)
+
+result *= num1  # same as saying result = result * num1
+print(result)
