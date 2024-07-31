0a058464240eaebf66abe39bbd931c1031fa7127
learnitlessons <162319132+learnitlessons@users.noreply.github.com>
2024-07-31 10:06:15 +0000

    added print lesson and exercise

---
+++ exercises/lesson1_print_basic_strings.py
@@ -0,0 +1,12 @@
+# 1. Print a short story using multiple lines.
+# TODO: 
+
+# 2. Create a string that includes both single and double quotes.
+# TODO:
+
+# 3. Use escape characters to create a formatted list.
+# TODO:
+
+# 4. Combine different types of data (strings and numbers) in one print statement.
+# TODO:
+
---
+++ lessons/1.0. Printin with Pythong.py
@@ -0,0 +1,16 @@
+print("Hello, World!")
+
+print("I'm learning Python!")
+print("This is so much fun!")
+print("I can't wait to learn more!")
+
+print('This is a string with single quotes.')
+print("This is a string with double quotes.")
+
+print("He said, 'Python is awesome!'")
+print('She replied, "I totally agree!"')
+
+print("First line\nSecond line")
+print("This\tis\ttabbed")
+
+print("My name is", "Vit", "and I am", 30, "years old.")
