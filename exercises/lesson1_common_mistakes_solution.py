827b99cb5b8722bb8f1483332395e5fda2f76446
learnitlessons <162319132+learnitlessons@users.noreply.github.com>
2024-08-01 08:26:12 +0000

    added lesson_common_mistakes_solution

---
+++ exercises/lesson1_common_mistakes_solution.py
@@ -0,0 +1,9 @@
+print('Let\'s practice finding errors!')
+print('Let\'s practice finding errors!')
+print('What\'s wrong with this line?')
+print('Fix me')
+print('What is missing here?')
+print("What do I use", "incorrectly here?")
+
+
+      
