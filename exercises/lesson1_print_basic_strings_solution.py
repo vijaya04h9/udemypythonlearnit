25425e8e05163eab1ed0e86b86b02124c130f9c1
vijaya04h9 <vijayendarreddy123@gmail.com>
2025-01-12 18:58:21 +0000

    my first committed

--- exercises/lesson1_print_basic_strings_solution.py
+++ exercises/lesson1_print_basic_strings_solution.py
@@ -2,17 +2,17 @@
 print("The Story of Python")
 print("-----------------")
 
-print("Python was created by Guido van Rossum.")
+print("it  was created by Guido van Rossum.")
 print("It was first released in 1991.")
 
 print("\nPython versions:")
-print("Python 1.0 - 1994")
-print("Python 2.0 - 2000")
-print("Python 3.0 - 2008")
+print("Python.. 1.0 - 1994")
+print("Python.. 2.0 - 2000")
+print("Python.. 3.0 - 2008")
 
 
 # 2. Create a string that includes both single and double quotes.
-print('Guido van Rossum once said, "Python is an experiment in how much freedom programmers need. Too much freedom and nobody can read another\'s code; too little and expressivness is endangered"')
+print('Guido van Rossumm once said, "Python is an experiment in how much freedom programmers need. Too much freedom and nobody can read another\'s code; too little and expressivness is endangered"')
 
 
 # 3. Use escape characters to create a formatted list.
@@ -20,5 +20,5 @@ print("The Zen of Python includes:\n\t1. Beutiful is better than ugly.\n\t2. Exp
 
 
 # 4. Combine different types of data (strings and numbers) in one print statement.
-print("Python was released in", 1991)
+print("Python was releasedd in", 1991)
 
