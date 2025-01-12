7c0805278d0c27734ef707cff2ee43a9676878c1
vijaya04h9 <vijayendarreddy123@gmail.com>
2025-01-12 23:39:16 +0000

    my solved solutions

--- exercises/lesson1_common_mistakes.py
+++ exercises/lesson1_common_mistakes.py
@@ -1,9 +1,9 @@
-print('Let's practice finding errors!')
-  print('Let's practice finding errors!')
-print'What's wrong with this line?'
-Print('Fix me')
-print('What is missing here?'
-      print("What do I use" +, + "incorrectly here?")
+print('Let\'s practice finding errors!')
+print('Let\'s practice finding errors!')
+print('What\'s wrong with this line?')
+print('Fix me')
+print('What is missing here?')
+print("What do I use" , "incorrectly here?")
 
 
       
